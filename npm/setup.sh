8c0dbe8c17fe4483fc2c4d673d89056ff452d5b8
Mimi Flynn <mimi.flynn@effectiveui.com>
2016-03-06 14:28:24 -0500

    cleanup npm dependencies

--- npm/setup.sh
+++ npm/setup.sh
@@ -2,11 +2,17 @@
 
 echo "Installing NPM dependencies"
 
+echo "- Installing Express"
+sudo npm install -g express
+
 echo " - Installing Dev Tools"
 sudo npm install -g grunt grunt-cli bower
 
-echo " - Installing MEAN.io"
-sudo npm install -g mean-cli
+echo " - Installing Forever"
+sudo npm install -g forever
 
-echo " - Installing IonicFramework"
-sudo npm install -g cordova ionic
+# echo " - Installing MEAN.io"
+# sudo npm install -g mean-cli
+
+# echo " - Installing IonicFramework"
+# sudo npm install -g cordova ionic
